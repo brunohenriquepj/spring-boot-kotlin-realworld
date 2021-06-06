08689a8f9cdf49655e7c63ef619d44a0ca94f646
Bruno Pereira <bruno.henriquepj@gmail.com>
2021-06-06 00:38:20 -0300

    Fix test

--- src/test/kotlin/com/example/realworld/unit/adapter/UpdateUserRequestDataAdapterTest.kt
+++ src/test/kotlin/com/example/realworld/unit/adapter/UpdateUserRequestDataAdapterTest.kt
@@ -2,23 +2,31 @@ package com.example.realworld.unit.adapter
 
 import com.example.realworld.adapter.implementation.UpdateUserRequestDataAdapterImpl
 import com.example.realworld.entity.User
+import com.example.realworld.util.builder.common.StringBuilder
 import com.example.realworld.util.builder.user.UpdateUserRequestDataBuilder
 import io.kotest.matchers.equality.shouldBeEqualToComparingFields
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Test
+import org.springframework.security.crypto.password.PasswordEncoder
 
 class UpdateUserRequestDataAdapterTest {
-    private val adapter = UpdateUserRequestDataAdapterImpl(mockk())
+    private val passwordEncoderMock: PasswordEncoder = mockk()
+    private val adapter = UpdateUserRequestDataAdapterImpl(passwordEncoderMock)
 
     @Test
     fun `toUser should return User`() {
         // arrange
         val request = UpdateUserRequestDataBuilder().build()
 
+        val passwordEncoded = StringBuilder().build()
+        every { passwordEncoderMock.encode(request.password) } returns passwordEncoded
+
         val expected = User(
             email = request.email,
             userName = request.userName,
-            password = request.password,
+            password = passwordEncoded,
             bio = request.bio,
             image = request.image
         )
@@ -27,6 +35,10 @@ class UpdateUserRequestDataAdapterTest {
         val actual = adapter.toUser(request)
 
         // assert
+        verify(exactly = 1) {
+            passwordEncoderMock.encode(request.password)
+        }
+
         actual shouldBeEqualToComparingFields expected
     }
 }
