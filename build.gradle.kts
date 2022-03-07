add6327609dac5700438a82af0f4b7e0c6691083
Bruno Pereira <bruno.henriquepj@gmail.com>
2022-03-06 21:18:07 -0300

    Bump plugin.spring from 1.5.31 to 1.6.10 (#63) (#75)
    Bumps plugin.spring from 1.5.31 to 1.6.10.
    
    ---
    updated-dependencies:
    - dependency-name: plugin.spring
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.springframework.boot") version "2.6.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.5.31"
+    kotlin("plugin.spring") version "1.6.10"
     kotlin("plugin.jpa") version "1.6.10"
     jacoco
     id("com.adarshr.test-logger") version "3.1.0"
